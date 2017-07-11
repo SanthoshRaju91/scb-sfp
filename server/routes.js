c0e70c226b787d53bcc4ce257ed04f5a1bb7ead7
umesh-kulkarni <umesh.playstore@gmail.com>
2017-07-11 20:43:02 +0530

    added services api/config to get and post config file

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ import {
     Router
 } from 'express';
 import fs from 'fs';
-import config from '../config';
+import config from '../config.json';
 
 const routes = new Router;
 const {
@@ -18,4 +18,16 @@ routes.post('/submit', (req, res) => {
     });
 });
 
+routes.get('/config', (req, res) => {
+    res.json(config);
+});
+
+routes.post('/config', (req, res) => {
+    fs.writeFile('config.json', JSON.stringify(req.body), 'utf8', (err) => {
+        if (err) throw err;
+        res.json({
+            transactionSuccess: true
+        });
+    });
+})
 export default routes;
